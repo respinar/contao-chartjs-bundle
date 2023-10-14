45954d5e7706a3297b730a747a179830f49d967f
Hamid Peywasti <hamid@respinar.com>
2023-10-14 21:35:07 +0330

    Rename ChartJsExtension to ChartjsExtension

--- src/DependencyInjection/RespinarChartjsExtension.php
+++ src/DependencyInjection/RespinarChartjsExtension.php
@@ -10,18 +10,18 @@ declare(strict_types=1);
  * @license MIT
  */
 
-namespace Respinar\ChartJsBundle\DependencyInjection;
+namespace Respinar\ChartjsBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class RespinarChartJsExtension extends Extension
+class RespinarChartjsExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
         $loader->load('services.yaml');
     }
 }
