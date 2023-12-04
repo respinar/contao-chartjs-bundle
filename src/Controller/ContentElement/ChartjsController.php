4a3636004b33e36807ce72adf60c7942a2d08739
Hamid Peywasti <hamid@respinar.com>
2023-12-04 22:48:35 +0330

    define template for Contao 5

--- src/Controller/ContentElement/ChartjsController.php
+++ src/Controller/ContentElement/ChartjsController.php
@@ -11,7 +11,7 @@ use Contao\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-#[AsContentElement(category: 'chartjs')]
+#[AsContentElement(category: 'chartjs', template: 'ce_chartjs')]
 class ChartjsController extends AbstractContentElementController
 {
     protected function getResponse(Template $template, ContentModel $model, Request $request): Response
