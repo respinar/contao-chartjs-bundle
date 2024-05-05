328dcc2e0d51e910f5ac670040726727847e27ea
Hamid Peywasti <hamid@respinar.com>
2024-05-05 12:56:14 +0330

    Update copyright notice

--- src/Controller/ContentElement/ChartjsController.php
+++ src/Controller/ContentElement/ChartjsController.php
@@ -2,27 +2,33 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of Contao Chart.js Bundle.
+ *
+ * (c) Hamid Peywasti 2023-2024 <hamid@respinar.com>
+ *
+ * @license MIT
+ */
+
 namespace Respinar\ContaoChartjsBundle\Controller\ContentElement;
 
 use Contao\ContentModel;
 use Contao\CoreBundle\Controller\ContentElement\AbstractContentElementController;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsContentElement;
-use Contao\Template;
+use Contao\CoreBundle\Twig\FragmentTemplate;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 #[AsContentElement(category: 'chartjs', template: 'ce_chartjs')]
 class ChartjsController extends AbstractContentElementController
 {
-    protected function getResponse(Template $template, ContentModel $model, Request $request): Response
+    protected function getResponse(FragmentTemplate $template, ContentModel $model, Request $request): Response
     {
 
         $template->type = $model->chartjs_type;
         $template->data = $model->chartjs_data;
 
-
-        $GLOBALS['TL_HEAD'][] = Template::generateScriptTag('bundles/respinarchartjs/js/chart.umd.js', false, null);
-        $GLOBALS['TL_BODY'][] = Template::generateStyleTag('bundles/respinarchartjs/css/style.css', false, null);
+        $GLOBALS['TL_JAVASCRIPT'][] = 'bundles/respinarcontaochartjs/js/chart.umd.js|static';
 
         return $template->getResponse();
     }
