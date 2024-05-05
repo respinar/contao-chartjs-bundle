a74ccd7529c8dd88f2ca4087e58294d8f1d8483d
Hamid Peywasti <hamid@respinar.com>
2024-05-05 12:55:41 +0330

    Update copyright notice

--- contao/config/config.php
+++ contao/config/config.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 /*
- * This file is part of Contao Chart.js.
+ * This file is part of Contao Chart.js Bundle.
  *
- * (c) Hamid Peywasti 2023 <hamid@respinar.com>
+ * (c) Hamid Peywasti 2023-2024 <hamid@respinar.com>
  *
  * @license MIT
  */
